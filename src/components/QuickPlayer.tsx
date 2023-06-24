759e9aa9331b4013dd9eacbe0b401aed4d499e76
Jakub Beránek <jakub.beranek@vsb.cz>
2023-06-24 18:36:55 +0200

    Use WebAudio to play recordings
    This allows us to authenticate the route for streaming audio files

--- src/components/QuickPlayer.tsx
+++ src/components/QuickPlayer.tsx
@@ -3,28 +3,48 @@ import { Sample } from "../api/api";
 import PlayCircleIcon from "@mui/icons-material/PlayCircle";
 import PauseCircleIcon from "@mui/icons-material/PauseCircle";
 import { useState } from "react";
-import { audioUrl } from "../common/audio";
+import { createAudioFilesApi } from "../common/service";
+import { useLoggedUser } from "../common/user";
 
 export function QuickPlayer(props: { sample: Sample }) {
   const [audioPlaying, setAudioPlaying] = useState<boolean>(false);
-  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+  const [audio, setAudio] = useState<AudioBufferSourceNode | null>(null);
 
+  const userToken = useLoggedUser()?.token;
   const start = () => {
     setAudioPlaying(true);
 
-    const audio = new Audio();
-    const source = document.createElement("source");
-    source.setAttribute("src", audioUrl(props.sample));
-    // TODO FIX FOR Safari
-    audio.append(source);
-    setAudio(audio);
-    audio.addEventListener("ended", () => setAudioPlaying(false));
-    audio.play();
+    const api = createAudioFilesApi();
+    api
+      .getAudioAudioFilesFilenameGet(props.sample.audio_files[0].path, {
+        responseType: "arraybuffer",
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      })
+      .then((response) => {
+        const audioCtx = new AudioContext();
+        const source = audioCtx.createBufferSource();
+        source.connect(audioCtx.destination);
+        setAudio(source);
+
+        audioCtx.decodeAudioData(
+          response.data,
+          (buffer) => {
+            source.buffer = buffer;
+            source.start(0);
+            source.addEventListener("ended", () => setAudioPlaying(false));
+          },
+          (error) => {
+            console.error(error);
+          }
+        );
+      });
   };
 
   const stop = () => {
     setAudioPlaying(false);
-    audio?.pause();
+    audio?.stop();
   };
 
   return (
