cd1b3dd49934ba3a5a8db5a5115fda6f2221fa44
Maros112358 <b.maros.1999@gmail.com>
2023-06-24 09:27:00 +0000

    run prettier

--- src/common/user.ts
+++ src/common/user.ts
@@ -1,8 +1,8 @@
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 import { atom, useRecoilValue } from "recoil";
 import { getRecoil, setRecoil } from "recoil-nexus";
 
-const LOGGED_USER: string = "loggedUser"
+const LOGGED_USER: string = "loggedUser";
 
 export type LoggedUser = {
   name: string;
@@ -12,12 +12,14 @@ export type LoggedUser = {
 
 const loggedUser = atom<LoggedUser | null>({
   key: LOGGED_USER,
-  default: Cookies.get(LOGGED_USER) ? JSON.parse(Cookies.get(LOGGED_USER) as string) : null
+  default: Cookies.get(LOGGED_USER)
+    ? JSON.parse(Cookies.get(LOGGED_USER) as string)
+    : null,
 });
 
 export function setLoggedUser(user: LoggedUser | null) {
   setRecoil(loggedUser, user);
-  Cookies.set(LOGGED_USER, JSON.stringify(user)); 
+  Cookies.set(LOGGED_USER, JSON.stringify(user));
 }
 
 export function useLoggedUser(): LoggedUser | null {
