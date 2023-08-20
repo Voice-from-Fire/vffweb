9f3c2856a5adf6ec65e091b9f08baaa3ea1667c9
Jakub Stejskal <jakubstejskal22@gmail.com>
2023-08-20 20:56:34 +0200

    Improve sample deletion
    - extract DeleteButton component
    - add deletion confirmation dialog
    - make the button smaller and less prone to accidental click

---
+++ src/components/DeleteButton.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+
+function DeleteButton({ onDelete }: { onDelete: () => void }) {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleConfirm = () => {
+    setOpen(false);
+    onDelete();
+  };
+
+  return (
+    <div>
+      <IconButton aria-label="delete" color="warning" onClick={handleClickOpen}>
+        <DeleteIcon />
+      </IconButton>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Delete Confirmation"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to delete this recording?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleConfirm} color="warning" autoFocus>
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default DeleteButton;
